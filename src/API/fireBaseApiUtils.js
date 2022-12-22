42f6381c2429666b74f73519ddca2829d5f1dff6
vykis <vykintaspilipauskas@gmail.com>
2022-12-22 11:37:01 +0200

    feat: created config file for api

--- src/API/fireBaseApiUtils.js
+++ src/API/fireBaseApiUtils.js
@@ -1,4 +1,6 @@
-export const API_KEY = "AIzaSyB2Q5AJP_rrTsdG816PpZ52SHgUUzxWLDU";
+import { firbaseApiKey } from "./ConfigApi";
+
+export const API_KEY = firbaseApiKey;
 export const FIREBASE_USERS_URL =
   "https://cheap-netflix-default-rtdb.firebaseio.com/user";
 export const FIRBASE_LOGIN_SINGUP_URL =
--- src/API/movieApiUtils.js
+++ src/API/movieApiUtils.js
@@ -1,4 +1,6 @@
-export const API_KEY = "cf59878deb4339519e182c488dbc4a31";
+import { movieApiKey } from "./ConfigApi";
+
+export const API_KEY = movieApiKey;
 export const MAIN_URL = "https://api.themoviedb.org/3/";
 export const tvGanreUrl = `${MAIN_URL}genre/tv/list?api_key=${API_KEY}&language=en-US`;
 export const discoverTvUrl = `${MAIN_URL}discover/tv?api_key=${API_KEY}&with_genres=`;
