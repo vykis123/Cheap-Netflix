e668bb1b5274321cc35ebfed2adf9fae2dd6b173
vykis <vykintaspilipauskas@gmail.com>
2023-01-01 21:02:20 +0200

    feat: added env file

--- src/API/fireBaseApiUtils.js
+++ src/API/fireBaseApiUtils.js
@@ -1,7 +1,5 @@
-//import { firbaseApiKey } from "./ConfigApi";
+export const API_KEY = process.env.REACT_APP_FIRBASE_API;
 
-// export const API_KEY = firbaseApiKey;
-export const API_KEY = process.env.REACT_APP_FIRBASE_API_KEY;
 export const FIREBASE_USERS_URL =
   "https://cheap-netflix-default-rtdb.firebaseio.com/user";
 export const FIRBASE_LOGIN_SINGUP_URL =
